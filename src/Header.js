172893db6bd8b8881496a259524880c21d085fee
Juan Fonseca <juanof2000@gmail.com>
2024-09-21 22:17:44 -0300

    Adiciona um componente de header para a aplicação

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/add">Adicionar Medicamento</Link></li>
+          
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
